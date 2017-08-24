0c397a93d522fc0b8f59b4db27159dcb5e2e439e
JGPNS <PlusNinetySix@users.noreply.github.com>
2017-08-24 12:43:09 -0500

    Uploaded source code
    GuessingGame2 is a backup of before the loop code.

---
+++ GuessingGame2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class GuessingGame2 {
+
+	public static void main(String[] args) 
+	{
+		Scanner cin=new Scanner(System.in);
+		System.out.println("Please guess a number between 1 and 100:");
+		int n=cin.nextInt();
+		int a=1;
+		int b=100;
+		int random=(int)(a+b*Math.random());
+		
+		if (n<random)
+		{
+			System.out.println("Too low!");
+		}
+		else if (n>random)
+		{
+			System.out.println("Too high!");
+		}
+		else if (n==random)
+		{
+			System.out.println("You got it!");
+		}
+		cin.close();
+	}
+
+}
